e00ecdf1503deef000698dba8acf44ee2a346cfa
Raymond Parag <r_parag@msn.com>
2016-01-29 19:20:57 +0100

    GetWebPage function complete

--- Information Retrieval/main.cpp
+++ Information Retrieval/main.cpp
@@ -7,25 +7,66 @@
 //
 
 #include <iostream>
+//#include "htmlstreamparser.h"
 #include <curl/curl.h>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    
-    cout << "Hello, World!\n";
-    return 0;
+static string readBufferHTML;
+
+static size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp)
+{
+    size_t realsize = size * nmemb;
+    readBufferHTML.append((char*)buffer, realsize);
+    return realsize;
 }
 
-char *GetWebPage(char *myur)
+char *GetWebPage(char *myurl)
 {
     CURL *easyhandle = curl_easy_init();
+    CURLcode results;
+    readBufferHTML.clear();
     
+    curl_easy_setopt(easyhandle, CURLOPT_URL, myurl);
+    curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, &readBufferHTML);
     
-    return 0;
+    results = curl_easy_perform(easyhandle);
+    curl_easy_cleanup(easyhandle);
+   
+    char stringHTML[readBufferHTML.size()];
+    strcpy(stringHTML, readBufferHTML.c_str());
+    char *HTMLpointer = stringHTML;
+    
+    if(strlen(HTMLpointer) != 0)
+    {
+        cout << "Successfully obtained HTML!";
+        return HTMLpointer; //Somewhere deallocate pointer at end of function when used --> free(....)
+    }
+    else
+    {
+        cout << "Failed obtaining HTML!";
+        return 0;
+    }
 }
 
 char *GetLinksFromWebPage(char *myhtmlpage, char *myurl)
 {
-    
+    //hiermee aan de slag
     return 0;
 }
+
+
+int main(int argc, const char * argv[]) {
+    
+    curl_global_init( CURL_GLOBAL_ALL );
+    
+    char url[] = "http://tweakers.net/";
+    //char *test = GetWebPage(url);
+    GetWebPage(url);
+    
+    cout << " \n";
+    return 0;
+}
+
+
+
