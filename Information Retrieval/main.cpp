f51903367d9fe47d2c00de684b591f7152cbbe4d
Raymond Parag <r_parag@msn.com>
2016-01-29 15:05:46 +0100

    Initial Commit

---
+++ Information Retrieval/main.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  Information Retrieval
+//
+//  Created by Raymond Parag on 29-01-16.
+//  Copyright © 2016 Raymond Parag. All rights reserved.
+//
+
+#include <iostream>
+#include <curl/curl.h>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    cout << "Hello, World!\n";
+    return 0;
+}
+
+char *GetWebPage(char *myur)
+{
+    CURL *easyhandle = curl_easy_init();
+    
+    
+    return 0;
+}
+
+char *GetLinksFromWebPage(char *myhtmlpage, char *myurl)
+{
+    
+    return 0;
+}
